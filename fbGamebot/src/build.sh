41fa1f795cc829f126346ca858c467849aaa126a
qibinyi <qibinyi@qibinyis-imac.ubisoft.org>
2019-03-07 16:30:04 +0800

    m

--- fbGamebot/src/build.sh
+++ fbGamebot/src/build.sh
@@ -4,9 +4,9 @@ echo "gitcommit:" $GIT_COMMIT
 
 TARGET=botserver
 DIST_FOLDER="/Volumes/WORK/WORK/STACK/UMC-Common-Service"
-GOOS=linux GOARCH=arm64 go build -ldflags "-X main.GitCommit=$GIT_COMMIT"  -o $TARGET
+GOOS=linux GOARCH=amd64 go build -ldflags "-X main.GitCommit=$GIT_COMMIT"  -o $TARGET
 
-mv -f $TARGET  $DIST_FOLDER 
+mv -f $TARGET  $DIST_FOLDER/
 
 
 
