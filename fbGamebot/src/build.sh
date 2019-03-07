b2a41fc2c99641aff9d9322de494ca2255bff381
qibinyi <qibinyi@qibinyis-imac.ubisoft.org>
2019-03-07 15:57:46 +0800

    m

--- fbGamebot/src/build.sh
+++ fbGamebot/src/build.sh
@@ -2,7 +2,7 @@ GIT_COMMIT=$(git rev-list -1 HEAD)
 
 echo $GIT_COMMIT
 
-go build -ldflags "-X main.GitCommit=$GIT_COMMIT"  -o botserver
+go build -ldflags "-X main.GitCommit=$GIT_COMMIT"  -o /Volumes/WORK/WORK/STACK/UMC-Docs/gamebot/botserver
 
 
 
