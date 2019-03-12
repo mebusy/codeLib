764db7fadf964c5b56348800a3dc518c1e1960aa
qibinyi <qibinyi@qibinyis-imac.ubisoft.org>
2019-03-12 17:56:14 +0800

    go proxy

---
+++ GoProxy/src/proxy.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+    "fmt"
+    // "os"
+    "flag"
+)
+
+var listenPort = flag.Int( "p" , 31280 , "listen port" ) 
+var targetURL = flag.String( "t", "127.0.0.1:3128" ,   "host:port" ) 
+
+func main() {
+	// Listen on TCP port 2000 on all available unicast and
+	// anycast IP addresses of the local system.
+	l, err := net.Listen("tcp", fmt.Sprintf( ":%d" , *listenPort )  )
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+	for {
+		// Wait for a connection.
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+        // log.Printf( "new client, %+v" , conn )
+		// Handle the connection in a new goroutine.
+		// The loop then returns to accepting, so that
+		// multiple connections may be served concurrently.
+        client := conn 
+        target, err := net.Dial("tcp", *targetURL )
+        if err != nil {
+            log.Fatal("could not connect to target", err)
+        }
+        defer target.Close()
+        // Echo all incoming data.
+        // client -> target
+        go func() { io.Copy(target, client) }()
+        // target -> client 
+        go func() { io.Copy(client, target) }()
+        // Shut down the connection.
+        defer conn.Close()
+	}
+}
