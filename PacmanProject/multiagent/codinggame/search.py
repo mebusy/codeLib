32b7cf9d84c8bfcc099944b6d55d6a7d9ebf2103
Qi Binyi <qibinyi@Qis-mini.local>
2020-11-20 21:33:29 +0800

    m

--- PacmanProject/multiagent/codinggame/search.py
+++ PacmanProject/multiagent/codinggame/search.py
@@ -1,7 +1,6 @@
 #!/usr/local/bin/python3
 import time
 
-import numpy as np
 import heapq
 
 class PriorityQueue:
@@ -70,6 +69,7 @@ class Problem(object):
         self.spell_ids = spell_ids
         self.spell_repeatable = spell_repeatable
         self.nSpell = len( spells )
+        self.ALL_CASTABLE = (1,) * self.nSpell
 
     def getStartState(self):
         return self.startState
@@ -77,34 +77,43 @@ class Problem(object):
         return self.goalState
     def getHeuristic(self, state):
         h = 0
-        diff = state.ingredients - self.goalState.ingredients
-        for i,v in enumerate(diff):
+        for i in range(4 ):
+            v = state.ingredients[i] - self.goalState.ingredients[i]
             if v < 0:
                 h += -v * (1)
         return h
     def isGoalState(self, state):
-        diff = state.ingredients - self.goalState.ingredients
-        return np.all( diff >= 0 )
+        for i in range(4 ):
+            v = state.ingredients[i] - self.goalState.ingredients[i]
+            if v < 0:
+                return False
+        return True
     def getSuccessors( self, state ):
         suc = []
 
         # action REST -1
-        if state.spell_castable.sum() < self.nSpell  : # NEW
-            suc.append( ( State( state.ingredients+np.zeros(4), np.ones( self.nSpell  ) ), -1,1, 1 ) )
+        # if state.spell_castable.sum() < self.nSpell  : # NEW
+        for castable in state.spell_castable:
+            if castable == 0: 
+                suc.append( ( State( state.ingredients, self.ALL_CASTABLE ), -1,1, 1 ) )
+                break
 
         # other spell
         for i,spell in enumerate( self.spells ):
             if state.spell_castable[i] == 1:
                 max_cast_times = self.spell_repeatable[i] and 10 or 1
                 for j in range(max_cast_times):
-                    ingredients = state.ingredients + spell*(j+1)
-                    if ingredients.sum() <= 10 and np.all( ingredients >= 0 ):
-                        spell_castable = state.spell_castable + np.zeros( self.nSpell )
-                        spell_castable[i] = 0 # casted
-                        suc.append( (State( ingredients, spell_castable ),self.spell_ids[i], j+1,  1 ))
+                    hasNegative = False
+                    ingredients = tuple( (state.ingredients[ii] + spell[ii]*(j+1) for ii in range(4) ))
+                    for v in ingredients:
+                        if v < 0:
+                            hasNegative = True
+                            break
+                    # print( state.ingredients , spell,  ingredients , sum( ingredients ) )
+                    if sum( ingredients ) <= 10 and not hasNegative :
+                        suc.append( (State( ingredients, state.spell_castable[:i]+ (0,) +state.spell_castable[i+1:]  ),self.spell_ids[i], j+1,  1 ))
                     else:
                         break
-
         return suc
 
         
@@ -113,30 +122,29 @@ class Problem(object):
 class State(object):
     def __init__(self, ingredients, spell_castable ):
         # all positive
-        self.ingredients = np.array(ingredients)
+        self.ingredients = ingredients
         self.spell_castable = spell_castable
         # print(self.ToString())
     def ToString(self):
-        return ( tuple(self.ingredients), tuple(self.spell_castable) )
+        return self.ingredients + self.spell_castable
 
 
 
 if __name__ == '__main__':
     start = time.time()
 
-    for i in range(100):
+    for _ in range(100):
         fringe = PriorityQueue()
 
-        spells =  [[2, 0, 0, 0], [-1, 1, 0, 0], [0, -1, 1, 0], [0, 0, -1, 1], [-4,0,1,0] ] 
+        spells =  [(2, 0, 0, 0), (-1, 1, 0, 0), (0, -1, 1, 0), (0, 0, -1, 1), (-4,0,1,0) ] 
         spell_ids = [1+i for i in range( len(spells) ) ]
         nSpell = len(spells)
         problem = Problem( 
-            State([2,1,0,0], np.ones(nSpell)), 
-            State(np.array([1,1,3,1]), np.ones(nSpell)),
-            # State(np.array([0,4,0,0]), np.ones(nSpell)),
-            np.array( spells), 
-            np.array(spell_ids),
-            [True]* nSpell
+            State((2,1,0,0), (1,)*nSpell), 
+            State((1,1,3,1), (1,)*nSpell),
+            tuple( spells), 
+            tuple(spell_ids),
+            (True,)* nSpell
             )
         # print( problem.getHeuristic( State([3,0,0,0]) ) )
         print( graphSearch( problem, fringe )  )
