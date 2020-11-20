038a641f255f4b4887c43895fb9dba9767b2102e
Qi Binyi <qibinyi@Qis-mini.local>
2020-11-20 20:11:00 +0800

    m

--- PacmanProject/multiagent/codinggame/search.py
+++ PacmanProject/multiagent/codinggame/search.py
@@ -1,4 +1,6 @@
 #!/usr/local/bin/python3
+import time
+
 import numpy as np
 import heapq
 
@@ -23,11 +25,12 @@ class PriorityQueue:
 def graphSearch( problem, fringe):
 
     closed = {}
+    nExpand = 0
 
     def buildPath( last_path_info ) :
         ret = []
         while last_path_info[0] is not None:
-            ret.append( last_path_info[1] )
+            ret.append( last_path_info[1:] )
             last_path_info = closed[ last_path_info[0] ]
         ret.reverse()
         return ret
@@ -43,26 +46,30 @@ def graphSearch( problem, fringe):
             return []
         state , path_info  , costs  = fringe.pop()
         if problem.isGoalState(state):
+            print( "nExpand", nExpand )# , file=sys.stderr, flush=True)
             return buildPath( path_info )
 
-        print (state , " -> ")
+        # print state , " -> "
         # expand
-        if state not in closed :
-            closed[state] = path_info
-            for sucState , action, cost  in problem.getSuccessors( state ):
-                fringe.push( ( sucState , (state,action) , costs + cost ),
+        if state.ToString() not in closed :
+            closed[state.ToString()] = path_info
+            for sucState , action, cast_times, cost  in problem.getSuccessors( state ):
+                fringe.push( ( sucState , (state.ToString(),action, cast_times ) , costs + cost ),
                     costs + cost + problem.getHeuristic( sucState )  )
-                print ("\t <- " , sucState)
+                nExpand += 1
+                # print "\t <- " , sucState
 
     return []
 
 
 class Problem(object):
-    def __init__(self, startState, goalState, spells, spell_ids):
+    def __init__(self, startState, goalState, spells, spell_ids, spell_repeatable):
         self.startState = startState
         self.goalState = goalState
         self.spells = spells
         self.spell_ids = spell_ids
+        self.spell_repeatable = spell_repeatable
+        self.nSpell = len( spells )
 
     def getStartState(self):
         return self.startState
@@ -71,44 +78,70 @@ class Problem(object):
     def getHeuristic(self, state):
         h = 0
         diff = state.ingredients - self.goalState.ingredients
-        for v in diff:
+        for i,v in enumerate(diff):
             if v < 0:
-                h += -v
+                h += -v * (1)
         return h
     def isGoalState(self, state):
         diff = state.ingredients - self.goalState.ingredients
         return np.all( diff >= 0 )
     def getSuccessors( self, state ):
         suc = []
-        # action REST -1
-        if state.spell_castable.sum() < 4: # NEW
-            suc.append( ( State( state.ingredients+np.zeros(4), np.ones(4) ), -1, 1 ) )
 
+        # action REST -1
+        if state.spell_castable.sum() < self.nSpell  : # NEW
+            suc.append( ( State( state.ingredients+np.zeros(4), np.ones( self.nSpell  ) ), -1,1, 1 ) )
+
+        # other spell
         for i,spell in enumerate( self.spells ):
             if state.spell_castable[i] == 1:
-                ingredients = state.ingredients + spell
-                if ingredients.sum() <= 10 and np.all( ingredients >= 0 ):
-                    spell_castable = state.spell_castable + np.zeros(4)
-                    spell_castable[i] = 0 # casted
-                    suc.append( (State( ingredients, spell_castable ),self.spell_ids[i], 1 ))
+                max_cast_times = self.spell_repeatable[i] and 10 or 1
+                for j in range(max_cast_times):
+                    ingredients = state.ingredients + spell*(j+1)
+                    if ingredients.sum() <= 10 and np.all( ingredients >= 0 ):
+                        spell_castable = state.spell_castable + np.zeros( self.nSpell )
+                        spell_castable[i] = 0 # casted
+                        suc.append( (State( ingredients, spell_castable ),self.spell_ids[i], j+1,  1 ))
+                    else:
+                        break
 
         return suc
 
         
 
+
 class State(object):
-    def __init__(self, ingredients, spell_castable  ):
+    def __init__(self, ingredients, spell_castable ):
         # all positive
         self.ingredients = np.array(ingredients)
         self.spell_castable = spell_castable
+        # print(self.ToString())
+    def ToString(self):
+        return ( tuple(self.ingredients), tuple(self.spell_castable) )
+
 
 
 if __name__ == '__main__':
-    fringe = PriorityQueue()
-    problem = Problem( State([3,0,0,0], np.ones(4)), State(np.array([1,1,3,1]), np.ones(4)),
-        np.array( [[2, 0, 0, 0], [-1, 1, 0, 0], [0, -1, 1, 0], [0, 0, -1, 1]] ), np.array( [0,1,2,3] )  )
-    # print( problem.getHeuristic( State([3,0,0,0]) ) )
-    print( graphSearch( problem, fringe )  )
+    start = time.time()
+
+    for i in range(100):
+        fringe = PriorityQueue()
+
+        spells =  [[2, 0, 0, 0], [-1, 1, 0, 0], [0, -1, 1, 0], [0, 0, -1, 1], [-4,0,1,0] ] 
+        spell_ids = [1+i for i in range( len(spells) ) ]
+        nSpell = len(spells)
+        problem = Problem( 
+            State([2,1,0,0], np.ones(nSpell)), 
+            State(np.array([1,1,3,1]), np.ones(nSpell)),
+            # State(np.array([0,4,0,0]), np.ones(nSpell)),
+            np.array( spells), 
+            np.array(spell_ids),
+            [True]* nSpell
+            )
+        # print( problem.getHeuristic( State([3,0,0,0]) ) )
+        print( graphSearch( problem, fringe )  )
+
+    print( "use time: {}".format(time.time() - start))
 
 
 
