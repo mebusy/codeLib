f2534aec0429331646a72a2681167154de1e4ae3
mebusy <qibinyi@mebusys-imac.ubisoft.org>
2021-05-12 12:19:15 +0800

    m

--- pg_pong/softmax/fc_net_layer.py
+++ pg_pong/softmax/fc_net_layer.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+from layers import *
+from layer_utils import *
+
 def softmax( f, aggregate_axis = 0 ):
     # instead: first shift the values of f so that the highest number is 0:
     f_max = np.max(f, axis= aggregate_axis )
@@ -9,134 +12,7 @@ def softmax( f, aggregate_axis = 0 ):
     return f_exp / f_sum.reshape( f_sum.shape + (1,) )
 
 
-def affine_forward(x, w, b):
-    """
-    Computes the forward pass for an affine (fully-connected) layer.
-
-    The input x has shape (N, d_1, ..., d_k) where x[i] is the ith input.
-    We multiply this against a weight matrix of shape (D, M) where
-    D = \prod_i d_i
-
-    Inputs:
-    x - Input data, of shape (N, d_1, ..., d_k)
-    w - Weights, of shape (D, M)
-    b - Biases, of shape (M,)
-
-    Returns a tuple of:
-    - out: output, of shape (N, M)
-    - cache: (x, w, b)
-    """
-    out = x.reshape(x.shape[0], -1).dot(w) + b
-    cache = (x, w, b)
-    return out, cache
-
-
-def affine_backward(dout, cache):
-    """
-    Computes the backward pass for an affine layer.
-
-    Inputs:
-    - dout: Upstream derivative, of shape (N, M)
-    - cache: Tuple of:
-      - x: Input data, of shape (N, d_1, ... d_k)
-      - w: Weights, of shape (D, M)
-
-    Returns a tuple of:
-    - dx: Gradient with respect to x, of shape (N, d1, ..., d_k)
-    - dw: Gradient with respect to w, of shape (D, M)
-    - db: Gradient with respect to b, of shape (M,)
-    """
-    x, w, b = cache
-    dx = dout.dot(w.T).reshape(x.shape)
-    dw = x.reshape(x.shape[0], -1).T.dot(dout)
-    db = np.sum(dout, axis=0)
-    return dx, dw, db
-
-
-def relu_forward(x):
-    """
-    Computes the forward pass for a layer of rectified linear units (ReLUs).
-
-    Input:
-    - x: Inputs, of any shape
-
-    Returns a tuple of:
-    - out: Output, of the same shape as x
-    - cache: x
-    """
-    out = np.maximum(0, x)
-    cache = x
-    return out, cache
-
-
-def relu_backward(dout, cache):
-    """
-    Computes the backward pass for a layer of rectified linear units (ReLUs).
-
-    Input:
-    - dout: Upstream derivatives, of any shape
-    - cache: Input x, of same shape as dout
-
-    Returns:
-    - dx: Gradient with respect to x
-    """
-    x = cache
-    dx = np.where(x > 0, dout, 0)
-    return dx
-
-
-def softmax_loss(x, y):
-    """
-    Computes the loss and gradient for softmax classification.
-
-    Inputs:
-    - x: Input data, of shape (N, C) where x[i, j] is the score for the jth class
-      for the ith input.
-    - y: Vector of labels, of shape (N,) where y[i] is the label for x[i] and
-      0 <= y[i] < C
-
-    Returns a tuple of:
-    - loss: Scalar giving the loss
-    - dx: Gradient of the loss with respect to x
-    """
-    probs = np.exp(x - np.max(x, axis=1, keepdims=True))
-    probs /= np.sum(probs, axis=1, keepdims=True)
-    N = x.shape[0]
-    loss = -np.sum(np.log(probs[np.arange(N), y])) / N
-    dx = probs.copy()
-    dx[np.arange(N), y] -= 1
-    dx /= N
-    return loss, dx
-
-
-# ==================================================================
-
-def affine_relu_forward(x, w, b):
-    """
-    Convenience layer that perorms an affine transform followed by a ReLU
-
-    Inputs:
-    - x: Input to the affine layer
-    - w, b: Weights for the affine layer
-
-    Returns a tuple of:
-    - out: Output from the ReLU
-    - cache: Object to give to the backward pass
-    """
-    a, fc_cache = affine_forward(x, w, b)
-    out, relu_cache = relu_forward(a)
-    cache = fc_cache + (relu_cache, )
-    return out, cache
-
-
-def affine_relu_backward(dout, cache):
-    """
-    Backward pass for the affine-relu convenience layer
-    """
-    x, w, b , relu_cache = cache
-    da = relu_backward(dout, relu_cache)
-    dx, dw, db = affine_backward(da, (x, w, b ) )
-    return dx, dw, db
+# ===================
 
 
 def forwardpass_test():
@@ -177,8 +53,7 @@ def backpass_test( y ):
     # grads["b1"] = db1   # ignore bias
     return loss, grads
 
-# if __name__ == '__main__':
-def test():
+if __name__ == '__main__':
     D = 80 * 80
     H = 200 # number of hidden layer neurons
     C = 2 # for softmax
@@ -229,6 +104,4 @@ def test():
     loss, grads = backpass_test( y )
     print( loss, grads )
 
-if __name__ == '__main__':
-    pass
 
