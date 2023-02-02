de2fe833eb765efc772b8447d0d8110b11af1d52
mebusy <qibinyi@mebusys-iMac.ubisoft.org>
2023-02-02 18:51:56 +0800

    m

--- working_settings/makesoftlink.sh
+++ working_settings/makesoftlink.sh
@@ -7,8 +7,8 @@ set -e
 # for-loop get files in current directory
 
 for file in *; do
-    # if file is not a bash file, echo file
-    if [[ ! $file == *.sh ]]; then
+    # if file is not a bash file, and not a .bak file  echo file
+    if [[ $file != *.sh ]] && [[ $file != *.bak ]]; then
         # echo $file
         target_file=~/.$file
         # force solftlink to target file
