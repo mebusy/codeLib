cd2071af9f4d82e106e78642ab7dfe9a6cdcc1e2
qibinyi <qibinyi@Qis-Mac-mini-6.local>
2023-02-27 23:05:26 +0800

    m

--- working_editor_configs/makesoftlink_ed.sh
+++ working_editor_configs/makesoftlink_ed.sh
@@ -4,12 +4,17 @@
 
 set -e
 
+TARGET_DIR=/Volumes/WORK
+if [ "$(uname)" = "Linux" ]; then
+    TARGET_DIR=~/WORK
+fi
+
 # for-loop get files in current directory
 
 for file in *; do
     # if file is not a bash file, and not a directory
     if [[ ! $file == *.sh ]] && [[ ! -d $file ]]; then
-        target_file=/Volumes/WORK/.$file
+        target_file=${TARGET_DIR}/.$file
         # force solftlink to target file
         ln -sf $PWD/$file $target_file
         ls -l $target_file
