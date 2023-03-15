84a73ef595117b831c5ebcb434cb373812b4e082
mebusy <qibinyi@mebusys-iMac.ubisoft.org>
2023-03-15 17:56:23 +0800

    m

--- working_settings/makesoftlink.sh
+++ working_settings/makesoftlink.sh
@@ -8,7 +8,7 @@ set -e
 
 for file in *; do
     # if file is not a bash file, and not a .bak file  echo file
-    if [[ $file != *.sh ]] && [[ $file != *.bak ]]; then
+    if [[ $file != *.sh ]] && [[ $file != *.bak ]] && [[ $file != Homebrew.json ]]; then
         # echo $file
         target_file=~/.$file
         # force solftlink to target file
