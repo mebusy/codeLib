9868d6cd9e0e34cf416ebfc60c97778042ce5940
Qi Binyi <qibinyi@Qis-Mac-mini.local>
2024-06-12 23:59:11 +0800

    m

--- pg_pong/pg-pong3-softmax-gpt-3action.py
+++ pg_pong/pg-pong3-softmax-gpt-3action.py
@@ -10,7 +10,7 @@ import gym
 H = 200  # number of hidden layer neurons
 C = 3  # number of classes
 batch_size = 10  # every how many episodes to do a param update?
-learning_rate = 1e-4
+learning_rate = 1e-3
 gamma = 0.99  # discount factor for reward
 decay_rate = 0.99  # decay factor for RMSProp leaky sum of grad^2
 resume = False  # resume from previous checkpoint?
