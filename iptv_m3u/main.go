177778d033da4b67763eb4f3510aa8374effdb2f
Qi Binyi <qibinyi@Qis-Mac-mini.local>
2024-06-23 13:32:47 +0800

    m

--- iptv_m3u/main.go
+++ iptv_m3u/main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"strings"
 )
 
 func main() {
+	// http GET https://iptv-org.github.io/iptv/index.m3u
+	resp, err := http.Get("https://iptv-org.github.io/iptv/countries/cn.m3u")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	defer resp.Body.Close()
+	// read all
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	// read lines
+	lines := strings.Split(string(body), "\n")
+	for _, line := range lines {
+		fmt.Println(line)
+	}
+
 	fmt.Println("done")
 }
