a95be74f3cb3a44e5e1a6688a628e045175077e1
mebusy <golden_slime@hotmail.com>
2024-11-04 16:34:37 +0800

    m

--- archLinux/i3wmConf/makeSoftLink.sh
+++ archLinux/i3wmConf/makeSoftLink.sh
@@ -5,6 +5,11 @@ set -e
 echo make soft link
 
 src_path=$(cd "$(dirname ${BASH_SOURCE[0]} )" && pwd)/$(basename "$1")
+
 ln -sf ${src_path}/i3_config ~/.config/i3/config
+mkdir -p ~/.config/alacritty
+ln -sf ${src_path}/alacritty.toml ~/.config/alacritty/alacritty.toml
+
+
 
 
