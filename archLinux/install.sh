98bdf0c32035d0da02f1258fd0d3ce06ac5ef2c7
mebusy <golden_slime@hotmail.com>
2024-11-04 15:01:59 +0800

    * git config

--- archLinux/install.sh
+++ archLinux/install.sh
@@ -2,4 +2,14 @@
 
 set -e
 
+# update pacman
+sudo pacman -Syyu --noconfirm
+
+# git & config
+sudo pacman --noconfirm -Sy git less
+git config --global user.name "mebusy"
+git config --global user.email "golden_slime@hotmail.com"
+# linux only
+git config credential.helper store
+
 
