5da164b32e7e2482ca979c811c1f1576e6fc4795
mebusy <mebusy@mebusys-iMac.ubisoft.org>
2024-11-04 15:07:48 +0800

    m

--- archLinux/install.sh
+++ archLinux/install.sh
@@ -2,14 +2,31 @@
 
 set -e
 
-# update pacman
-sudo pacman -Syyu --noconfirm
+# the 1st param is instStep, default is 0
+instStep=${1:-0}
 
-# git & config
-sudo pacman --noconfirm -Sy git less
-git config --global user.name "mebusy"
-git config --global user.email "golden_slime@hotmail.com"
-# linux only
-git config credential.helper store
+
+
+
+# if instStep <= 0
+if [ $instStep -le 0 ]; then
+    # step 0
+    # update pacman
+    sudo pacman -Syyu --noconfirm
+fi
+
+
+if [ $instStep -le 1 ]; then
+    # step 1
+    # git & config
+    sudo pacman --noconfirm -Sy git less
+    git config --global user.name "mebusy"
+    git config --global user.email "golden_slime@hotmail.com"
+    # if is linux
+    if [ -f /etc/lsb-release ]; then
+        git --config credential.helper store
+    fi
+fi
+
 
 
