004753cdff0afb0c9987d9f75dfd8b5fc4848c27
mebusy <mebusy@mebusys-iMac.ubisoft.org>
2024-11-04 15:16:14 +0800

    m

--- archLinux/install.sh
+++ archLinux/install.sh
@@ -19,7 +19,7 @@ fi
 if [ $instStep -le 1 ]; then
     # step 1
     # git & config
-    sudo pacman --noconfirm -Sy git less
+    sudo pacman --noconfirm -S git less
     git config --global user.name "mebusy"
     git config --global user.email "golden_slime@hotmail.com"
     # if is linux
@@ -28,5 +28,11 @@ if [ $instStep -le 1 ]; then
     fi
 fi
 
+if [ $instStep -le 2 ]; then
+    # step 2
+    # font & alacritty
+    sudo pacman --noconfirm -S noto-fonts-cjk wqy-zenhei adobe-source-han-sans-otc-fonts
+    sudo pacman --noconfirm -S alacritty picom fcitx fcitx-googlepinyin fcitx-configtool
+fi
 
 
