1d625594f45775f57637b8338b7e78c206f84d56
mebusy <golden_slime@hotmail.com>
2025-12-30 10:22:54 +0800

    m

--- working_editor_configs/makesoftlink_ed.sh
+++ working_editor_configs/makesoftlink_ed.sh
@@ -24,49 +24,58 @@ if [ -n "$1" ]; then
     TARGET_DIR=$1
 fi
 
-# for-loop get files in current directory
+RamDisk_DIR="/Volumes/RamDisk"
 
-for file in *; do
-    # if file name is "ctags"
-    if [[ $file == ctags ]]; then
-        target_file=~/.ctags
-        # force solftlink to target file
-        ln -sf $PWD/$file $target_file
-        ls -l $target_file
-    elif [[ $file == "ycm_extra_conf.py" ]]; then
-        echo skip $file
-    elif [[ ! $file == *.sh ]] && [[ ! -d $file ]]; then
-        # if file is not a bash file, and not a directory
-        target_file=${TARGET_DIR}/.$file
-        # force solftlink to target file
-        ln -sf $PWD/$file $target_file
-        ls -l $target_file
+
+# for dir in both TARGET_DIR and RamDisk_DIR
+for  DIR in $TARGET_DIR $RamDisk_DIR; do
+    # if dir not exists, create it
+    if [ -d $DIR ]; then
+
+        # for-loop get files in current directory
+        for file in *; do
+            # if file name is "ctags"
+            if [[ $file == ctags ]]; then
+                target_file=~/.ctags
+                # force solftlink to target file
+                ln -sf $PWD/$file $target_file
+                ls -l $target_file
+            elif [[ $file == "ycm_extra_conf.py" ]]; then
+                echo skip $file
+            elif [[ ! $file == *.sh ]] && [[ ! -d $file ]]; then
+                # if file is not a bash file, and not a directory
+                target_file=${DIR}/.$file
+                # force solftlink to target file
+                ln -sf $PWD/$file $target_file
+                ls -l $target_file
+            fi
+        done
+
+
+        for file in eslint_prettier/JS/*; do
+            if [[ ! $file == *.sh ]] && [[ ! $file == *.eslintrc.js ]]  && [[ ! -d $file ]]; then
+                # remove path from $file
+                fileName=${file##*/}
+                target_file=${DIR}/$fileName
+                # force solftlink to target file
+
+                # ln -sf $PWD/$file $target_file
+                # ls -l $target_file
+                #
+                # # if the file is not starts with dot ".", then hidden it's target file
+                # if [[ ! $fileName == .* ]]; then
+                #     # if is Darwin
+                #     if [ "$(uname)" = "Darwin" ]; then
+                #         # use -h to hidden a symbol link
+                #         chflags -h hidden $target_file
+                #     fi
+                # fi
+                rm -f $target_file
+            fi
+        done
+
+
     fi
 done
-
-
-for file in eslint_prettier/JS/*; do
-    if [[ ! $file == *.sh ]] && [[ ! $file == *.eslintrc.js ]]  && [[ ! -d $file ]]; then
-        # remove path from $file
-        fileName=${file##*/}
-        target_file=${TARGET_DIR}/$fileName
-        # force solftlink to target file
-
-        # ln -sf $PWD/$file $target_file
-        # ls -l $target_file
-        #
-        # # if the file is not starts with dot ".", then hidden it's target file
-        # if [[ ! $fileName == .* ]]; then
-        #     # if is Darwin
-        #     if [ "$(uname)" = "Darwin" ]; then
-        #         # use -h to hidden a symbol link
-        #         chflags -h hidden $target_file
-        #     fi
-        # fi
-        rm -f $target_file
-    fi
-done
-
-
 
 
