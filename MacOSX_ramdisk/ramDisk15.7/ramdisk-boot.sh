b5b746d7189abe93db8babf4f0daec3045c99f54
mebusy <mebusy@mebusys-iMac.ubisoft.org>
2026-01-03 00:35:49 +0800

    m

--- MacOSX_ramdisk/ramDisk15.7/ramdisk-boot.sh
+++ MacOSX_ramdisk/ramDisk15.7/ramdisk-boot.sh
@@ -6,22 +6,27 @@ RAMDISK_SIZE=8192 # Size in MB.
 RAMDISK_NAME="RamDisk"
 MOUNT_POINT="/Volumes/${RAMDISK_NAME}"
 
-log() { echo "$(date) [ramdisk-boot] $*"; }
+function create_ramdisk {
+    if mount | grep -q "${MOUNT_POINT}"; then
+        exit 0
+    fi
 
-logger -t ramdisk-boot "start $(date)"
+    let RAMDISK_BLOCKSIZE=2048*${RAMDISK_SIZE} # Size in blocks.
+    # DEV=`hdiutil attach -nomount "ram://${RAMDISK_BLOCKSIZE}"`
+    # format
+    # diskutil erasevolume APFSX "${RAMDISK_NAME}" `hdiutil attach -nomount "ram://${RAMDISK_BLOCKSIZE}"`
+    DEV=$(hdiutil attach -nomount "ram://${RAMDISK_BLOCKSIZE}")
+    diskutil erasevolume APFSX "${RAMDISK_NAME}" $DEV
+    
+    # mount
+    if mount | grep -q "on ${MOUNT_POINT} "; then
+        echo "RAMDisk already mounted at ${MOUNT_POINT}"
+    else
+        diskutil mount -mountPoint "${MOUNT_POINT}" $DEV
+    fi
+}
 
-if /sbin/mount | grep -q "${MOUNT_POINT}"; then
-    log "Ramdisk already mounted at ${MOUNT_POINT}, skipping creation."
-    exit 0
-fi
 
-let RAMDISK_BLOCKSIZE=2048*${RAMDISK_SIZE} # Size in blocks.
-# format, HFS+ 在开机期间更稳定
-# diskutil erasevolume APFSX "${RAMDISK_NAME}" `hdiutil attach -nomount "ram://${RAMDISK_BLOCKSIZE}"`
-DEV=$(hdiutil attach -nomount "ram://${RAMDISK_BLOCKSIZE}")
-diskutil erasevolume APFSX "${RAMDISK_NAME}" $DEV
+create_ramdisk
 
-logger -t ramdisk-boot "done $(date)"
-
-exit 0  # ensure quit
 
